ecb6580bb7b13159110b1697b796dfed26d3a683
MariusTamosaitis <you@example.com>
2018-11-30 13:34:44 +0200

    šeimos gydytojo ligų aprašai

--- database.php
+++ database.php
@@ -164,6 +164,12 @@ class MySQLDB {
         return $result;
     }
 
+    function getAllIlnesses(){
+        $query = "Select CONCAT(pavadinimas,' ', ligos_kodas) AS liga from ".TBL_LIGA."";
+        $result = mysqli_query($this->connection, $query);
+        return $result;
+    }
+
     function getId($id){
         $query = "SELECT id_VARTOTOJAS FROM ".TBL_VARTOTOJAS." WHERE id_VARTOTOJAS= ".$id;
         $result = mysqli_query($this->connection, $query);
